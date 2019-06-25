a29e70e47e17612f9fd56f97df7489ba64079a09
Trevor Castillo <40138120+Truephoria@users.noreply.github.com>
2019-06-25 01:36:49 -0700

    Update RoseRing.js

--- RoseRing.js
+++ RoseRing.js
@@ -1,25 +1,21 @@
-
-
 function RoseRing(x,y,r){
  this.x = x;
  this.y = y;
  this.r = r;
   
-this.movement = function(){
-
-
+this.movement = function()
+{
 
 };
 
 
-this.show = function(){
+this.show = function()
+  {
   beginShape();
   stroke(155,155,155);
   strokeWeight(3);
   ellipse(this.x,this.y,this.r);
   endShape();
-};
-
-
+  };
 
 };
