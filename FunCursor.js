7b2327d142facfbc40fe6c5834885858149717eb
Trevor Castillo <40138120+Truephoria@users.noreply.github.com>
2019-06-25 01:37:28 -0700

    Update FunCursor.js

--- FunCursor.js
+++ FunCursor.js
@@ -1,78 +1,56 @@
 var radius = 0;
 var alph = 255;
 
-
-
  function FunCursor(){
-   this.a = alph;
+ this.a = alph;
  this.r = radius;
  this.posX = mouseX;
  this.posY = mouseY;
- 
- 
- 
- 
- 
- this.grow = function(){
-   
-  if(radius < 150){
+
+ this.grow = function()
+ {
+ if(radius < 150)
+   {
  radius += 10
- console.log('grow');
- if(radius>=75){
+ if(radius>=75)
+     {
  alph -= 20
- }
+     }
    }
-   
-   
-   
-   
    this.stopGrow();
  }
  
+ this.show = function()
+ {
+   push()
+   noFill();
+   stroke(255,0,0,alph);
+   strokeWeight(3);
+   ellipse(mouseX ,mouseY, radius, radius);
+   pop();
  
+   push()
+   noFill();
+   stroke(255,0,0,alph);
+   strokeWeight(2);
+   ellipse(mouseX ,mouseY, radius - 10, radius - 10);
+   pop();
  
- 
- this.show = function(){
- push()
- noFill();
- stroke(255,0,0,alph);
- strokeWeight(3);
- ellipse(mouseX ,mouseY, radius, radius);
- pop();
- 
- push()
- noFill();
- stroke(255,0,0,alph);
- strokeWeight(2);
- ellipse(mouseX ,mouseY, radius - 10, radius - 10);
- pop();
- 
- push()
- noFill();
- stroke(255,0,0,alph);
- strokeWeight(1);
- ellipse(mouseX,mouseY, radius - 20, radius - 20);
- pop();
+   push()
+   noFill();
+   stroke(255,0,0,alph);
+   strokeWeight(1);
+   ellipse(mouseX,mouseY, radius - 20, radius - 20);
+   pop();
  }
  
- this.stopGrow = function(){
- 
- if(radius == 150){
-   
-   radius =0;
-   alph = 255;
-   fCursorGrowInt--
+ this.stopGrow = function()
+ {
+ if(radius == 150)
+   {
+     radius =0;
+     alph = 255;
+     fCursorGrowInt--
    }
- 
- }
- 
- //this.resetCursor = function(){
- 
- //alph = 255;
- //radius = 0;
- //fCursorGrowInt--;
- 
- //}
- 
- }
- 
+ } 
+}
